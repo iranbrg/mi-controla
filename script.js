196833100642315f442df502a2925d79c8e842b5
Felipe Góes <felipecgoes4@gmail.com>
2020-07-01 21:21:51 -0300

    final

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let popUpInfo = document.getElementById('popUpInfo')
+let popUpRetirada = document.getElementById ('popUpRetirada')
+let fundoPop = document.getElementById('fundoPop')
+
+let testeMain = document.querySelector('main').children
+
+function exibirPopInfo () {
+    popUpInfo.style.display = "grid";
+    fundoPop.style.display = "block";
+}
+
+function retirarPopInfo () {
+    popUpInfo.style.display = "none";
+    fundoPop.style.display = "none";
+}
+
+function exibirPopRetirada () {
+    popUpRetirada.style.display = "grid";
+    fundoPop.style.display = "block";
+
+}
+ 
+function retirarPopRetirada () {
+    popUpRetirada.style.display = "none";
+    fundoPop.style.display = "none";
+
+}
+
+
