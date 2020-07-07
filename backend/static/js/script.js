53ea9f1e40eab808df36967b4217e71098af9cb3
azyn1 <gbrl.almeida01@gmail.com>
2020-07-07 00:18:24 -0300

    adaptação dos templates

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -1,31 +1,27 @@
-
-
 let popUpInfo = document.getElementById('popUpInfo')
-let popUpRetirada = document.getElementById ('popUpRetirada')
+let popUpRetirada = document.getElementById('popUpRetirada')
 let fundoPop = document.getElementById('fundoPop')
 
 let testeMain = document.querySelector('main').children
 
-function exibirPopInfo () {
+function exibirPopInfo() {
     popUpInfo.style.display = "grid";
     fundoPop.style.display = "block";
 }
 
-function retirarPopInfo () {
+function retirarPopInfo() {
     popUpInfo.style.display = "none";
     fundoPop.style.display = "none";
 }
 
-function exibirPopRetirada () {
+function exibirPopRetirada() {
     popUpRetirada.style.display = "grid";
     fundoPop.style.display = "block";
 
 }
- 
-function retirarPopRetirada () {
+
+function retirarPopRetirada() {
     popUpRetirada.style.display = "none";
     fundoPop.style.display = "none";
 
 }
-
-
--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,26 @@
 from backend import app
+from flask import render_template
+
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/estoque')
+def estoque():
+    return render_template('estoque.html', title='Estoque') 
+
+@app.route('/adicionar', methods=['POST','GET'])
+def adicionar():
+    return render_template('adicionar.html', title='Adicionar ') 
+
+@app.route('/historico')
+def historico():
+    return render_template('historico.html', title='Histórico') 
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    return render_template('login.html', title='Login') 
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html', title='Registro') 
+
+
