f67bb997e5c71090b41f7ffc644c24170e23bbff
Felipe Góes <felipecgoes4@gmail.com>
2020-07-10 21:03:04 -0300

    dados PopUp Js funcional

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -2,31 +2,17 @@ let popUpInfo = document.getElementById('popUpInfo')
 let popUpRetirada = document.getElementById('popUpRetirada')
 let fundoPop = document.getElementById('fundoPop')
 
-let testeMain = document.querySelector('main').children
 
 
 // A variável "info_produto" é recebida por meio do atributo "data-info" e posteriormente deverá ser passada do JS para o HTML para que as informações do produto sejam exibidas nos pop-ups
-<<<<<<< HEAD
-
-=======
 // var info = document.getElementById("idDoBotao").getAttribute("data-info");
->>>>>>> ab5aab459e0b419dc4636df4a51669bee07ef3dc
 
-function exibirPopInfo() {
-    popUpInfo.style.display = "grid";
-    fundoPop.style.display = "block";
-}
 
 function retirarPopInfo() {
     popUpInfo.style.display = "none";
     fundoPop.style.display = "none";
 }
 
-function exibirPopRetirada() {
-    popUpRetirada.style.display = "grid";
-    fundoPop.style.display = "block";
-
-}
 
 function retirarPopRetirada() {
     popUpRetirada.style.display = "none";
@@ -34,9 +20,47 @@ function retirarPopRetirada() {
 
 }
 
-var a = document.getElementsByClassName('detalheItem')
-var info = a[0].getAttribute("data-info");
+var botoes = document.getElementsByClassName('detalheItem')
 
-console.log(a);
-console.log(info);
+//não está reconhecendo o data-atribute como vetor. Quando reconhecer a função vai funcionar
 
+for (i=0; i < botoes.length; i++){
+   document.getElementById(botoes[i].id).addEventListener('click', dados);
+}
+
+
+function dados(){
+    popUpInfo.style.display = "grid";
+    fundoPop.style.display = "block";
+    var vDados = this.getAttribute("data-info");
+    document.getElementById('1').textContent = vDados[0];
+    document.getElementById('2').textContent = vDados[1];
+    document.getElementById('3').textContent = vDados[2];
+    document.getElementById('4').textContent = vDados[3];
+    document.getElementById('5').textContent = vDados[4];
+    document.getElementById('6').textContent = vDados[5];
+}
+
+console.log(botoes);
+
+//Para o pop-up de retirada
+
+var botoesRetirada = document.getElementsByClassName('bt')
+
+for (i=0; i < botoesRetirada.length; i++){
+    document.getElementById(botoesRetirada[i].id).addEventListener('click', dadosRetirada);
+ }
+
+function dadosRetirada(){
+    popUpRetirada.style.display = "grid";
+    fundoPop.style.display = "block";
+    var vDadosR = this.getAttribute("data-info2");
+    document.getElementById('1.1').textContent = vDadosR[0];
+    document.getElementById('2.1').textContent = vDadosR[1];
+    document.getElementById('3.1').textContent = vDadosR[2];
+    document.getElementById('4.1').textContent = vDadosR[3];
+    document.getElementById('5.1').textContent = vDadosR[4];
+    console.log(vDadosR);
+}
+
+//o id é o código de barras para os botões de retirar
