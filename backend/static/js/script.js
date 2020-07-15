ba2ae8101cd46d99665bfab748cc2ab947b41007
iranbrg <iran.brg@gmail.com>
2020-07-15 14:27:31 -0300

    Vetor corrigido
    O vetor passado do macros.html para o scrips.js agora é convertido numa string, antes de ser passado como atributo, e dps reconvertido num vetor no scripts.js

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -2,22 +2,14 @@ let popUpInfo = document.getElementById('popUpInfo')
 let popUpRetirada = document.getElementById('popUpRetirada')
 let fundoPop = document.getElementById('fundoPop')
 
-
-
-// A variável "info_produto" é recebida por meio do atributo "data-info" e posteriormente deverá ser passada do JS para o HTML para que as informações do produto sejam exibidas nos pop-ups
-// var info = document.getElementById("idDoBotao").getAttribute("data-info");
-
-
 function retirarPopInfo() {
     popUpInfo.style.display = "none";
     fundoPop.style.display = "none";
 }
 
-
 function retirarPopRetirada() {
     popUpRetirada.style.display = "none";
     fundoPop.style.display = "none";
-
 }
 
 var botoes = document.getElementsByClassName('detalheItem')
@@ -28,20 +20,18 @@ for (i=0; i < botoes.length; i++){
    document.getElementById(botoes[i].id).addEventListener('click', dados);
 }
 
-
 function dados(){
     popUpInfo.style.display = "grid";
     fundoPop.style.display = "block";
-    var vDados = this.getAttribute("data-info");
+    var vDados = this.getAttribute("data-info").split(",");
     document.getElementById('1').textContent = vDados[0];
     document.getElementById('2').textContent = vDados[1];
     document.getElementById('3').textContent = vDados[2];
     document.getElementById('4').textContent = vDados[3];
     document.getElementById('5').textContent = vDados[4];
     document.getElementById('6').textContent = vDados[5];
+    console.log(vDados);
 }
-
-console.log(botoes);
 
 //Para o pop-up de retirada
 
@@ -54,7 +44,7 @@ for (i=0; i < botoesRetirada.length; i++){
 function dadosRetirada(){
     popUpRetirada.style.display = "grid";
     fundoPop.style.display = "block";
-    var vDadosR = this.getAttribute("data-info2");
+    var vDadosR = this.getAttribute("data-info2").split(",");
     document.getElementById('1.1').textContent = vDadosR[0];
     document.getElementById('2.1').textContent = vDadosR[1];
     document.getElementById('3.1').textContent = vDadosR[2];
