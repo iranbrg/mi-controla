06dcb15424e447c54fb3d3187b4b85ddc1d4637c
iranbrg <iran.brg@gmail.com>
2020-07-17 16:15:48 -0300

    Planilha de histórico terminada
    Novas funções atinentes ao histórico no arquivo pd.py

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, url_for, request, flash
 from backend import app
 from backend.forms import NovoProduto, RetirarProduto
-from backend.pd import novo_produto, leitura_estoque, retirar_produto
+from backend.pd import novo_produto, leitura_estoque, retirar_produto, leitura_historico
+from backend.datahora import hora
 
 @app.route('/', methods=['POST','GET'])
 @app.route('/estoque/', methods=['POST','GET'])
@@ -28,7 +29,7 @@ def adicionar():
 
 @app.route('/historico/')
 def historico():
-    return render_template('historico.html') 
+    return render_template('historico.html', hist=leitura_historico())
 
 @app.route('/login/', methods=['POST','GET'])
 def login():
